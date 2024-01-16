8bebbd0af5234f77c112b0b6b15f657514b33ef3
Gosula Sirisha <110887380+Sirishagosula@users.noreply.github.com>
2024-01-16 22:24:07 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ChipInput from './ChipInput';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <h1>Pick Users</h1> 
+      <ChipInput />
+    </div>
+  );
+};
+
+export default App;
